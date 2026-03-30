66dc76d3e04aec00eea145e9141c4006ace08f96
cnkulatunga <cnkulatunga@gmail.com>
2026-03-30 09:16:14 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: __dirname,
+  },
   images: {
     remotePatterns: [
       {
